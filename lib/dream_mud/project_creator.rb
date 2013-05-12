b8302233b794401a56eae875bf1476150e6a80ed
sparkymat <ajith@bullfin.ch>
2013-05-12 00:04:25 +0000

    Adding core executable and eventmachine

--- lib/dream_mud/project_creator.rb
+++ lib/dream_mud/project_creator.rb
@@ -16,8 +16,9 @@ class ProjectCreator
 
   def setup
     setup_folders
-    copy_core
+    copy_core_files
     create_config
+    create_app_file
   end
 
   def setup_folders
@@ -29,32 +30,23 @@ class ProjectCreator
     FileUtils.mkdir_p("#{@path}/config")
   end
 
-  def copy_core
+  def copy_core_files
     FileUtils.cp_r( Dir.glob("#{File.dirname(__FILE__)}/core/*.rb"), "#{@path}/core/" ) 
+    FileUtils.cp_r( "#{File.dirname(__FILE__)}/mud.rb", "#{@path}/" )
   end
 
   def create_config
     File.open("#{@path}/Gemfile", "w+") do |fp|
       fp.write("source 'https://rubygems.org'\n")
       fp.write("\n")
-      fp.write("gem 'dream_mud', '#{DreamMUD::VERSION}'\n\n")
+      fp.write("gem 'dream_mud', '#{DreamMUD::VERSION}'\n")
+      fp.write("gem 'eventmachine'\n")
+      fp.write("\n")
     end
+  end
 
-    File.open("#{@path}/config/application.rb", "w+") do |fp|
-      fp.write("dream_mud_path = '#{File.expand_path('../../lib',__FILE__)}'\n")
-      fp.write("$:.unshift(dream_mud_path\n")
-      fp.write("\n")
-      fp.write("require 'dream_mud/all'\n")
-      fp.write("\n")
-      fp.write("if defined?(Bundler)\n")
-      fp.write("  Bundler.require\n")
-      fp.write("end\n")
-      fp.write("\n")
-      fp.write("module #{@project_name}\n")
-      fp.write("  class Application < DreamMUD::Application\n")
-      fp.write("  end\n")
-      fp.write("end\n")
-      fp.write("\n")
+  def create_app_file
+    File.open("#{@path}/run.rb","w+") do |fp|
     end
   end
 end
